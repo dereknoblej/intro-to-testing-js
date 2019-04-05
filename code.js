dfeb92ec86b28c7f2f820b79275fd25a1e8589d8
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-04-05 14:09:22 -0500

    Excercise 6

--- code.js
+++ code.js
@@ -3,20 +3,20 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-// function sayHello(x) {
-//     return "Hello, " + x + "!";
-// }
-
 function sayHello(x) {
-    if (x === "Jane"){
-        return "Hello, Jane!"
-    }
-    else if (x === "Alex"){
-        return "Hello, Alex!"
-    } else if(x === "Pat"){
-        return "Hello, Pat!"
-    }
-    else {
-        return ""
-    }
+    return "Hello, " + x + "!";
 }
+
+// function sayHello(x) {
+//     if (x === "Jane"){
+//         return "Hello, Jane!"
+//     }
+//     else if (x === "Alex"){
+//         return "Hello, Alex!"
+//     } else if(x === "Pat"){
+//         return "Hello, Pat!"
+//     }
+//     else {
+//         return ""
+//     }
+// }
